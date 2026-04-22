96f1ff601f848f811740fe17d383cdfe65b8c5a9
Nathaniel Patla <njpatla@icloud.com>
2026-04-22 16:57:52 -0700

    Decrease Drive Speed

--- src/main/include/Configs.h
+++ src/main/include/Configs.h
@@ -31,7 +31,7 @@ class MAXSwerveModule {
         // These are example gains you may need to them for your own robot!
         .Pid(0.04, 0, 0)
         .VelocityFF(drivingVelocityFeedForward)
-        .OutputRange(-1, 1);
+        .OutputRange(-0.5, 0.5);
 
     return drivingConfig;
   }
